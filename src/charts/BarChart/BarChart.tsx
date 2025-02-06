3ebd5741c98c350dd029a3c5556a76c1f4f527cb
jewelcodes <omarmelghoul01@gmail.com>
2025-02-05 21:33:48 -0500

    bar: skeleton for parsing data series

--- src/charts/BarChart/BarChart.tsx
+++ src/charts/BarChart/BarChart.tsx
@@ -4,13 +4,47 @@
  */
 
 import "./BarChart.css";
+import "../Chartsy.css";
+import React, { ReactNode, useState } from "react";
 
-export function BarChart({ children }: Readonly<{ children: React.ReactNode }>) {
+export function BarChart({ children }: Readonly<{ children: ReactNode }>) {
+    const [maxValue, setMaxValue] = useState(0);
+    const [minValue, setMinValue] = useState(0);
+    const [series, setSeries] = useState([]);
+
+    const callback = (label: string, value: number) => {
+        const obj = { label, value };
+        if(series.some((s) => s.label === label)) return;
+    
+        setSeries(series => [...series, { label, value }]);
+        setMaxValue(Math.max(maxValue, value));
+        setMinValue(Math.min(minValue, value));
+    };
+
+    const childrenProps = React.Children.map(children, (child) => {
+        if(React.isValidElement(child)) {
+            return React.cloneElement(child, { callback: callback });
+        }
+    });
+
     return (
-        {children}
+        <>
+            {childrenProps}
+
+            <div className="chartsy-container">
+
+            </div>
+        </>
     );
 }
 
-export function BarDataSeries() {
+export function BarDataSeries({data, callback}: Readonly<
+    { data: Array<{label: string, value: number}>,
+    callback: (label: string, value: number) => void }>) {
+
+    data.forEach(({label, value}) => {
+        callback(label, value);
+    });
+
     return null;
 }
