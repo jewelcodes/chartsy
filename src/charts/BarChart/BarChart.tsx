5c000400d5a0c1ad50150201d91088540816e48f
jewelcodes <omarmelghoul01@gmail.com>
2025-02-05 20:54:28 -0500

    bar: skeleton for bar chart component

---
+++ src/charts/BarChart/BarChart.tsx
@@ -0,0 +1,16 @@
+/*
+ * Chartsy - elegant, customizable, and interactive standalone charts for React
+ * Omar Elghoul, 2025
+ */
+
+import "./BarChart.css";
+
+export function BarChart({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        {children}
+    );
+}
+
+export function BarDataSeries() {
+    return null;
+}
