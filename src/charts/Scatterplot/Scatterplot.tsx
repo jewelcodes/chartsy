ab2066ee6ccc400a464d69bb8b79e173e8ebc504
jewelcodes <omarmelghoul01@gmail.com>
2025-02-10 17:43:54 -0500

    scatter: file skeleton for scatterplots

---
+++ src/charts/Scatterplot/Scatterplot.tsx
@@ -0,0 +1,12 @@
+/*
+ * Chartsy - elegant, customizable, and interactive standalone charts for React
+ * Omar Elghoul, 2025
+ */
+
+"use client";
+"use strict";
+
+import "./BarChart.css";
+import "../Chartsy.css";
+import React, { ReactNode, useState, useMemo } from "react";
+
