c5954e9060f27321f5841d8ae2236591d0704b57
jewelcodes <omarmelghoul01@gmail.com>
2025-02-10 17:45:58 -0500

    scatter: interfaces for scatterplot props

--- src/charts/Scatterplot/Scatterplot.tsx
+++ src/charts/Scatterplot/Scatterplot.tsx
@@ -10,3 +10,24 @@ import "./BarChart.css";
 import "../Chartsy.css";
 import React, { ReactNode, useState, useMemo } from "react";
 
+export interface ScatterplotProps {
+    children: ReactNode;
+    live?: boolean;
+    toggle?: boolean;
+    xlabels?: boolean;
+    ylabels?: boolean;
+    labelColor?: string;
+    axis?: boolean;
+    axisColor?: string;
+    width?: number;
+    height?: number;
+    xgrid?: boolean;
+    ygrid?: boolean;
+    gridColor?: string;
+};
+
+export interface ScatterplotData {
+    x: number;
+    y: number;
+};
+
